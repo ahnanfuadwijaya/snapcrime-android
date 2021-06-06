ba8b7d3357944ceffa0de64b1f18cba44c90bf1c
Ahnan Fuadwijaya <ahnanfuadwijaya@gmail.com>
2021-06-06 18:56:08 +0700

    add image base url

--- app/src/main/java/id/riverflows/snapcrime/util/UtilConstants.kt
+++ app/src/main/java/id/riverflows/snapcrime/util/UtilConstants.kt
@@ -1,6 +1,7 @@
 package id.riverflows.snapcrime.util
 
 object UtilConstants {
+    const val IMAGE_BASE_URL = "https://storage.googleapis.com/snapcrime.appspot.com"
     const val DEF_TYPE_RAW = "raw"
     const val BOTT_SHEET_PEEK_HEIGHT = 200
     const val MIME_TYPE_IMAGE = "image/*"
